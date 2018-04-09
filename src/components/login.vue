6b03a6888ea7d85fa8042a26ef7f0210230fc996
liukai <471091127@qq.com>
2018-04-09 20:38:02 +0800

    增加验证码

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,21 @@
 <template>
 <div class="login">
-  <div class="login-form">
+  <div class="login-form" @keyup.enter="login">
     <h1>{{ msg }}</h1>
     <el-form :model="formData" :rules="loginRules" ref="loginForm" label-width="80px">
       <el-form-item label="登录账号" prop="name">
-        <el-input v-model="formData.name" placeholder="请输入登录账号" auto-complete="off" clearable></el-input>
+        <el-input v-model.trim="formData.name" placeholder="请输入登录账号" clearable></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="formData.password" placeholder="请输入密码" auto-complete="off" clearable></el-input>
+        <el-input type="password" v-model.trim="formData.password" placeholder="请输入密码" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="验证码" prop="captcha">
+        <el-input v-model.trim="captcha" placeholder="请输入验证码" clearable>
+            <template slot="append"><div class="captcha" v-html="captchaImg" @click="getCaptcha"></div></template>
+        </el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="loginIn" class="login-in">登录</el-button>
+        <el-button type="primary" @click="login" class="login-in">登录</el-button>
         <span>没账号?</span>
         <el-button type="text" @click="()=>{this.$router.push('/register')}">去注册</el-button>
       </el-form-item>
@@ -20,9 +25,19 @@
 </template>
 
 <script>
+import qs from 'qs'
 export default {
   name: 'login',
   data () {
+    let validateCaptcha = (rule, value, callback) => {
+      if (this.captcha === '') {
+        callback(new Error('请输入验证码'))
+      } else if (this.captcha.toLowerCase() !== this.captchaText) {
+        callback(new Error('验证码错误!'))
+      } else {
+        callback()
+      }
+    }
     return {
       msg: '后台管理系统',
       formData: {
@@ -39,29 +54,58 @@ export default {
           required: true,
           message: '请输入密码',
           trigger: 'blur'
+        }],
+        captcha: [{
+          required: true,
+          validator: validateCaptcha,
+          trigger: 'blur'
         }]
-      }
+      },
+      captcha: '',
+      captchaImg: '',
+      captchaText: ''
     }
   },
+  mounted () {
+    this.getCaptcha()
+  },
   methods: {
-    loginIn () {
+    login () {
       this.$refs.loginForm.validate((valid) => {
         if (valid) {
-          let data = new FormData(this.formData)
-          this.axios.post(window.APIHOST + 'api/loginIn', data).then(res => {
-            if (res.data.result === 1) {
-              this.$message.success('登陆成功')
-              this.$router.push('/home')
-            } else {
+          let data = qs.stringify(this.formData)
+          this.$nextTick(() => {
+            this.axios.post(window.APIHOST + 'api/login', data).then(res => {
+              if (res.data.result === 1) {
+                this.$message.success('登陆成功')
+                this.$router.push('/home')
+              } else if (res.data.result === 2) {
+                this.$message.warning(res.data.msg)
+                this.getCaptcha()
+              } else {
+                this.$message.error('登录失败')
+                this.getCaptcha()
+              }
+            }).catch(() => {
               this.$message.error('登录失败')
-            }
-          }).catch(() => {
-            this.$message.error('登录失败')
+              this.getCaptcha()
+            })
           })
         } else {
           return false
         }
       })
+    },
+    getCaptcha () {
+      this.axios.get(window.APIHOST + 'api/captcha').then(res => {
+        if (res.data.result === 1) {
+          this.captchaImg = res.data.data
+          this.captchaText = res.data.text.toLowerCase()
+        }
+      })
+        .catch(err => {
+          console.log(err)
+        })
     }
   }
 }
@@ -69,4 +113,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss'>
+.captcha{
+  height:38px;
+  overflow:hidden;
+  cursor:pointer;
+}
 </style>
