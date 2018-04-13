1e115a9503a4f3aa34491fe0150545f8c5a5ea47
liukai <471091127@qq.com>
2018-04-13 17:04:51 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,12 @@ import Router from 'vue-router'
 import login from '@/components/login'
 import register from '@/components/register'
 import layout from '@/components/layout'
+import notFound from '@/components/notFound'
 import home from '@/components/home'
 import personalInfo from '@/components/personalInfo'
 import articleManage from '@/components/articleManage'
+import newArticle from '@/components/newArticle'
+import articleDetail from '@/components/articleDetail'
 Vue.use(Router)
 
 export default new Router({
@@ -39,8 +42,29 @@ export default new Router({
           path: '/articleManage',
           name: 'articleManage',
           component: articleManage
+        },
+        {
+          path: '/newArticle',
+          name: 'newArticle',
+          component: newArticle,
+          meta: {
+            group: 'articleManage'
+          }
+        },
+        {
+          path: '/articleDetail/:id',
+          name: 'articleDetail',
+          component: articleDetail,
+          meta: {
+            group: 'articleManage'
+          }
         }
       ]
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      component: notFound
     }
   ]
 })
