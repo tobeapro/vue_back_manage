1d06e23b293ba18e7f4a58fc7b9d69f01aa66290
liukai <471091127@qq.com>
2018-05-07 20:58:13 +0800

    404

--- src/components/notFound.vue
+++ src/components/notFound.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="not-found">
+    <h1>404</h1>
     <el-button type="primary" @click="()=>{this.$router.push('/home')}">返回首页</el-button>
   </div>
 </template>
@@ -14,4 +15,11 @@ export default {
 }
 </script>
 <style lang="scss">
+.not-found{
+  margin-top:10vh;
+  text-align:center;
+  h1{
+    line-height:60px;
+  }
+}
 </style>
